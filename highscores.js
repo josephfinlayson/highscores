64141a9c0a3b6c4945577b736bfc1944c3e60fec
dgmp88 <dgmprichard@gmail.com>
2014-05-21 17:04:10 +0100

    Simplified some stuff

--- highscores.js
+++ highscores.js
@@ -9,7 +9,6 @@ if (Meteor.isServer) {
         return Meteor.methods({
             // Meteor.call('removeAllPosts')
             // From the console, this will remove all stored posts.
-
             removeAllPosts: function() {
                 return Highscores.remove({});
             }
@@ -37,6 +36,7 @@ if (Meteor.isServer) {
             a[n] = post;
             n++
         });
+        console.log
         return a;
     });
 
@@ -48,14 +48,10 @@ if (Meteor.isServer) {
         var user = this.params.user;
         var a = {};
         a.exists = false;   
-        var collection = Highscores.find({user : user});
-        collection.forEach (
-            function(post) {
-                console.log("for: " + post.user);
-                if (user == post.user) {
-                    a.exists = true;
-                }
-        })
+        var collection = Highscores.find({"user" : user});
+        if (collection.count()>0) {
+            a.exists = true;
+        }
         return a;
     });
 
@@ -71,15 +67,10 @@ if (Meteor.isServer) {
         var id_to_update = false;
         
         if (user && score) {
-            var collection = Highscores.find({user : user});
-            collection.forEach(
-                function(post) {
-                    if (post.user === user) {
-                        id_to_update = post._id;
-                    }
-                }
-            );
-
+            var collection = Highscores.find({"user" : user}).fetch();
+            if (collection.length != 0) {
+                id_to_update = collection[0]._id;
+            }
             // inserts user only if the user does not already exist
             if (id_to_update) {
                 Highscores.update({
@@ -96,7 +87,4 @@ if (Meteor.isServer) {
             }
         }
     });
-
-
-
 }
